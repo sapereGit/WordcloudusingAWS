a8de85413796ec1364b364b55a05f79457a2fb90
sapereGit <sapere@globallingua.ca>
2023-01-24 14:54:29 -0500

    s3bucketaccess

--- app.py
+++ app.py
@@ -3,13 +3,27 @@ __author__ = "Kevin shah(Sapere/GL)"
 __license__ = "MIT"
 __email__ = "kevin@globallingua.ca"
 __maintainer__ = "Kevin shah(Sapere/GL)"
+try:
+        
+    import json
+    import requests
+    import boto3
+    print("All imports ok ...")
+except Exception as e:
+    print("Error Imports : {} ".format(e))
 
-import json
-import requests
 
 def handler(event, context):
 
     # TODO implementation
+    s3_client = boto3.client('s3')
+    s3_clientobj = s3_client.get_object(Bucket='lambdacontainerexecute', Key='tp-data.txt')
+
+    for line in s3_clientobj['Body'].iter_lines():
+        object = json.loads(line)
+        print(f"Name: {object['name']['s']} ID: {object['id']['n']}")
+    print("Welcome to SAPERE")
+    print("Lambda execution Completed...!")
     
     return {
         'headers': {'Content-Type' : 'application/json'},
